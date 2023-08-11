8ba810fc237552cb568d1a71f39259ae64817bfb
vikramm0907 <135581252+vikramm0907@users.noreply.github.com>
2023-08-11 23:19:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}   
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
